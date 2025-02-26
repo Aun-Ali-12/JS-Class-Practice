ddbf8bbdaf25c161ef347d1d0a53ea070d656fc6
Aun-Ali-12 <aunali29331@gmail.com>
2025-02-26 23:22:32 +0500

    Practiced 26/02/2025 JS Class Work

--- app.js
+++ app.js
@@ -25,3 +25,25 @@ console.log(5 % 12);   // not 2 but 5 (because the value in which the dividend i
 console.log(7 % 7);    // 0
 console.log(9 % 2);    //1
 console.log(24 % 9);   //6
+
+
+let chair = 15
+chair++; //output 16
+console.log(chair);
+
+let chair1 = 15
+++chair1; //output 16
+console.log(chair1); //output 16
+ 
+//no difference in both of the above but....
+
+let chair_final = 15
+chair_final_res = chair_final++;
+console.log(chair_final_res);  //output will be 15 because in this case the value will first print (Post increment mn baad mn add hota hai) and then add 1 to it
+
+
+let chairFinal = 19
+chairFinal_res = ++chairFinal;
+console.log(chairFinal_res);  //output will be 20 as it is pre-increment and in it the value will be added first and then will get print on this console.
+
+//same for pre and post decrement
